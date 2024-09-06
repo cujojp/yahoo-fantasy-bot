53ea0b25a1628f994d7c19f7e2c1ef310e06150b
Kaleb White <kaleb.white@yahooinc.com>
2024-09-05 21:24:42 -0700

    Working on build

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
     implementation("io.ktor:ktor-client-json-jvm:$ktorVersion")
     implementation("io.ktor:ktor-client-gson:$ktorVersion")
-    implementation("org.koin:koin-ktor:2.1.6")
+    implementation("io.insert-koin:koin-ktor:2.1.6")
     implementation(project(":shared"))
     testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
 }
