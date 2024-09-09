64a491298d1a5ba443202e1e27c56cf0a3f4f36c
Kaleb White <kaleb.white@yahooinc.com>
2024-09-08 21:27:50 -0700

    Add Ktor and Koin dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,4 +1,5 @@
 val ktorVersion = "2.2.3"
+val koinVersion = "3.1.2"
 val logbackVersion = "1.2.1"
 val kotlinVersion = "1.8.10"  // Define the Kotlin version for dependencies
 
@@ -29,18 +30,23 @@ repositories {
 }
 
 dependencies {
+    // Ktor server dependencies
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-serialization-gson:$ktorVersion")  // Use ktor-serialization-gson instead of ktor-gson
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
     implementation("io.ktor:ktor-client-json-jvm:$ktorVersion")
     implementation("io.ktor:ktor-client-gson:$ktorVersion")
-    implementation("io.insert-koin:koin-ktor:2.1.6")
+    // Koin dependencies for dependency injection
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
     implementation(project(":shared"))
     testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    // Other dependencies
+    implementation("ch.qos.logback:logback-classic:1.2.1")
 }
 
 sourceSets {
