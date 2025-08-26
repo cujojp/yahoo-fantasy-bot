4e0b8387b12b47e865b124a527e82dd89456b2c8
kwhite01 <kaleb.white@yahooinc.com>
2025-08-25 19:33:33 -0500

    Fix database URL handling for Railway deployment

--- shared/src/main/kotlin/com/landonpatmore/yahoofantasybot/shared/utils/models/EnvVariable.kt
+++ shared/src/main/kotlin/com/landonpatmore/yahoofantasybot/shared/utils/models/EnvVariable.kt
@@ -36,7 +36,15 @@ sealed class EnvVariable {
         object GroupMeBotId : Str(System.getenv("GROUP_ME_BOT_ID") ?: "", true)
         object DiscordWebhookUrl : Str(System.getenv("DISCORD_WEBHOOK_URL") ?: "", true)
         object SlackWebhookUrl : Str(System.getenv("SLACK_WEBHOOK_URL") ?: "", true)
-        object JdbcDatabaseUrl : Str(System.getenv("JDBC_DATABASE_URL") ?: System.getenv("DATABASE_URL") ?: "")
+        object JdbcDatabaseUrl : Str(
+            System.getenv("JDBC_DATABASE_URL").let { 
+                if (it == null || it.isEmpty() || it == "\$DATABASE_URL") {
+                    System.getenv("DATABASE_URL") ?: ""
+                } else {
+                    it
+                }
+            }
+        )
     }
 
     sealed class Integer(val variable: Int) : EnvVariable() {
